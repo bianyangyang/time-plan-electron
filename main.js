2226276dd2e863ffa99073098777788854dc284f
biany <biany@szinfinova.com>
2020-08-28 14:53:36 +0800

    '添加最小化、最大化、关闭功能'

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 
 let mainWindow;
 
 app.on("ready", () => {
-    Menu.setApplicationMenu(null)
-    mainWindow = new BrowserWindow({
-        frame: false,
-        width: 1288,
-        height: 780,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
-    const urlLocation = "http://localhost:3000";
-    mainWindow.loadURL(urlLocation);
-    mainWindow.webContents.openDevTools()
+  Menu.setApplicationMenu(null);
+  mainWindow = new BrowserWindow({
+    frame: false,
+    width: 1288,
+    height: 780,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  const urlLocation = "http://localhost:3000";
+  mainWindow.loadURL(urlLocation);
+  mainWindow.webContents.openDevTools();
+});
+
+ipcMain.on("window-min", function () {
+  mainWindow.minimize();
+});
+ipcMain.on("window-max", function () {
+  if (mainWindow.isMaximized()) {
+    mainWindow.restore();
+  } else {
+    mainWindow.maximize();
+  }
+});
+ipcMain.on("window-close", function () {
+  mainWindow.close();
 });
--- src/component/TitleRight/index.js
+++ src/component/TitleRight/index.js
@@ -1,19 +1,35 @@
 import React from "react";
 import "./index.less";
-
 import IconFont from "../Iconfont";
+
+const ipcRenderer = window.require("electron").ipcRenderer;
 
 function TitleRight() {
   return (
     <div className="titleRight">
       <div className="titleDragComponent"></div>
-      <div className="titleControlItem">
+      <div
+        className="titleControlItem"
+        onClick={() => {
+          ipcRenderer.send("window-min");
+        }}
+      >
         <IconFont type="icon-zuixiaohua" />
       </div>
-      <div className="titleControlItem">
+      <div
+        className="titleControlItem"
+        onClick={() => {
+          ipcRenderer.send("window-full");
+        }}
+      >
         <IconFont type="icon-zuidahua" />
       </div>
-      <div className="titleControlItem">
+      <div
+        className="titleControlItem"
+        onClick={() => {
+          ipcRenderer.send("window-close");
+        }}
+      >
         <IconFont type="icon-guanbi" />
       </div>
     </div>
